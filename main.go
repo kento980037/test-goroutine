2515762e53afb09eb78cc281a1445e1081aa66e2
kento980037 <kento_0037@yahoo.co.jp>
2023-05-28 16:19:05 +0900

    add go-routine

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 	result1 := func1()
-	result2 := func2(result1)
-	result3 := func3(result1)
 
-	fmt.Println(result2)
-	fmt.Println(result3)
+	// channel の初期化
+	// 2個のバッファを持った channel を作成
+	resChan := make(chan any, 2)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func2(result1, resChan, &wg)
+	go func3(result1, resChan, &wg)
+
+	wg.Wait()
+
+	// resChan channel への送信を終了し channel を閉じる
+	close(resChan)
+
+	// channel が閉じられるまでループする
+	for res := range resChan {
+		fmt.Println("res: ", res)
+	}
+
 	fmt.Println("took: ", time.Since(start))
 }
 
@@ -21,12 +38,14 @@ func func1() string {
 	return "result1"
 }
 
-func func2(s string) string {
+func func2(s string, reschan chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 50)
-	return "result2" + s
+	reschan <- "result2" + s
+	wg.Done()
 }
 
-func func3(s string) string {
+func func3(s string, reschan chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 100)
-	return "result3" + s
+	reschan <- "result3" + s
+	wg.Done()
 }
